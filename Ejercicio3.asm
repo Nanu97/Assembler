442db8139763bec30545ac3e2cdb6eb1de9f1008
Nanu <nahuel.rd97@gmail.com>
2026-04-02 21:46:51 -0300

    Update Ejercicio3.asm

--- Ejercicio3.asm
+++ Ejercicio3.asm
@@ -1,27 +1,29 @@
+; INGRESE UN TEXTO E IMPRIMALO CON LA PRIMER LETRA DE CADA PALABRA EN MAYUSCULA
+
 .8086
 .model small
 .stack 100h
 .data
 	mensaje1 db "Ingrese el texto:", 0dh, 0ah, 24h
-	saltador db 					 0dh, 0ah, 24h			; SALTADOR (VARIABLE AUXILIAR)
-	texto    db 					 255 dup (24h), 24h
-	textoMod db 					 255 dup (24h), 24h
+	saltarin db 0dh, 0ah, 24h			            ; SALTARIN (VARIABLE AUXILIAR)
+	texto    db 255 dup (24h), 24h					; TEXTO ORIGINAL
+	textoMod db 255 dup (24h), 24h					; TEXTO MODIFICADO
 .code
 	main proc
 	mov ax, @data
 	mov ds, ax
 
-	mov ah, 9						; SERVICIO DE IMPRESION
-	mov dx, offset mensaje1			; IMPRIMO mensaje1
-	int 21h
+	mov ah, 9						                ; SERVICIO DE IMPRESION
+	mov dx, offset mensaje1			                ; IMPRIMO mensaje1
+	int 21h											; EJECUTO
 
 	;CAJA DE CARGA
-	mov bx, 0 						; PONGO EL INDICE EN 0 (SOLO PUEDO USAR BX PARA IR A MEMORIA)
+	mov bx, 0 										; PONGO EL INDICE EN 0
 
 	carga:
-		mov ah, 1 					; SERVICIO DE ENTRADA POR TECLADO
-		int 21h
-		cmp al, 0dh 				; COMPARO CON ENTER PARA TERMINAR LA CARGA DE TEXTO
+		mov ah, 1 									; SERVICIO DE ENTRADA POR TECLADO
+		int 21h										; EJECUTO INT 21h
+		cmp al, 0dh 								; COMPARO CON ENTER PARA FINALIZAR CARGA
 		je finCarga
 		mov texto[bx], al
 		mov textoMod[bx], al
@@ -29,21 +31,21 @@
 	jmp carga
 
 	finCarga:
-		mov bx, 0 			 		; REINICIALIZO BX PARA VOLVER A RECORRER
+		mov bx, 0 			 						; REINICIALIZO BX PARA VOLVER A RECORRER
 
 	proceso:
-		cmp textoMod[bx], 24h		; CONDICION DE SALIDA
+		cmp textoMod[bx], 24h						; CONDICION DE SALIDA
 		je  finProceso
-		cmp bx, 0					; 1° LETRA. SALTAR A ETIQUETA QUE VERIFIQUE SI ES MAY O MIN
+		cmp bx, 0									; PRIMER LETRA
 		je  espacio
-		cmp textoMod[bx-1], ' '		; SI POSICION ANTERIOR ES UN ESPACIO, VERIFICAR SI ES MINUSCULA
+		cmp textoMod[bx-1], ' '						; SI POSICION ANTERIOR ES UN ESPACIO, VERIFICO SI ES MINUSCULA
 		je  espacio
 	aumenta:
-		inc bx
+		inc bx										; INCREMENTO EL INDICE
 	jmp proceso
 
 	espacio:
-		cmp textoMod[bx], 61h		; SI ES MINUSCULA, MODIFICAR
+		cmp textoMod[bx], 61h						; SI ES MINUSCULA, MODIFICAR
 		jb  aumenta
 		jae inicialMin
 
