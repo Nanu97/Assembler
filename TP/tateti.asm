7b7976deba2de842b010fcce53870f4ab3e86b96
Nanu <nahuel.rd97@gmail.com>
2026-06-10 20:08:00 -0300

    Update tateti.asm

--- TP/tateti.asm
+++ TP/tateti.asm
@@ -3,31 +3,31 @@
 .stack 100h
 .data
 
-;=========================
-; POSICION LOGICA
-;=========================
-filaTablero db 0
-columnaTablero db 0
+    ;FILA Y COLUMNA DONDE ESTÁ PARADO EL CURSOR
+    filTablero  db 0
+    colTablero  db 0
+    scanCode    db 0
 
-scanCode db ?
+    ;VARIABLES PARA MANEJAR QUIÉN ESTÁ JUGANDO
+    turno       db 0
+    fichaActual db 'X'
 
-;=========================
-; TABLERO (Quitamos los saltos de línea para controlarlos por código)
-;=========================
-fila1 db '[ ] [ ] [ ]', 24h
-fila2 db '[ ] [ ] [ ]', 24h
-fila3 db '[ ] [ ] [ ]', 24h
+    ;VARIABLES PARA DIBUJAR EL TABLERO
+    fila1       db '[ ] [ ] [ ]', 24h
+    fila2       db '[ ] [ ] [ ]', 24h
+    fila3       db '[ ] [ ] [ ]', 24h
 
 .code
-main proc
+
+    main proc
+
     mov ax, @data
     mov ds, ax
 
 gameLoop:
-    ;================================================
-    ; DIBUJAR TABLERO CENTRADO
-    ;================================================
-    ; Fila 1 (Fila 11, Columna 35)
+    ;DIBUJAMOS  EL TABLERO EN EL CENTRO DE LA PANTALLA
+
+    ;FILA 1 COORDENADAS: X=35 || Y=11
     mov ah, 02h
     mov bh, 00h
     mov dh, 11
@@ -37,7 +37,7 @@ gameLoop:
     mov ah, 9
     int 21h
 
-    ; Fila 2 (Fila 12, Columna 35)
+    ;FILA 2 COORDENADAS: X=35 || Y=12
     mov ah, 02h
     mov bh, 00h
     mov dh, 12
@@ -47,7 +47,7 @@ gameLoop:
     mov ah, 9
     int 21h
 
-    ; Fila 3 (Fila 13, Columna 35)
+    ;FILA 3 COORDENADAS: X=35 || Y=13
     mov ah, 02h
     mov bh, 00h
     mov dh, 13
@@ -57,45 +57,36 @@ gameLoop:
     mov ah, 9
     int 21h
 
-    ;=========================
-    ; CALCULAR POSICION VISUAL DEL CURSOR ('X')
-    ;=========================
-    ; Fila: 11 + filaTablero
-    mov dh, filaTablero
+    mov dh, filTablero
     add dh, 11
 
-    ; Columna: 35 + (columnaTablero * 4) + 1  <-- El +1 es para caer dentro del [ ]
-    mov al, columnaTablero
+    ;JUEGO CON LAS COORDENADAS DE LA BIOS PARA APUNTAR EL CURSOR EN LA POSICION CORRECTA:
+
+    mov al, colTablero
     mov bl, 4
     mul bl
-    add al, 36   ; 35 del centro + 1 del espacio interno del corchete
+    add al, 36   
     mov dl, al
-
-    ;=========================
-    ; POSICIONAR CURSOR Y DIBUJAR X
-    ;=========================
     mov ah, 02h
     mov bh, 00h
     int 10h
 
+    ;IMPRIMO FICHA DEL TURNO ACTUAL:
+
     mov ah, 2
-    mov dl, 'X'
+    mov dl, fichaActual  
     int 21h
 
-    ;=========================
-    ; LEER TECLA
-    ;=========================
+    ;LEO LA TECLA
     mov ah, 00h
     int 16h
     mov scanCode, ah
 
-    ;=========================
-    ; BORRAR X EN LA POSICION ACTUAL ANTES DE MOVER
-    ;=========================
-    ; Recalculamos la misma posición donde se dibujó
-    mov dh, filaTablero
+    ;CALCULO DE VUELTA LA POSICION EN PANTALLA PARA BORRAR TEMPORALMENTE LA 'X' o LA 'O':
+
+    mov dh, filTablero
     add dh, 11
-    mov al, columnaTablero
+    mov al, colTablero
     mov bl, 4
     mul bl
     add al, 36
@@ -105,66 +96,127 @@ gameLoop:
     mov bh, 00h
     int 10h
 
+    mov al, colTablero
+    mov bl, 4
+    mul bl
+    add al, 1
+    xor ah, ah
+    mov si, ax
+
+    cmp filTablero, 0
+    je leerF1
+    cmp filTablero, 1
+    je leerF2
+    jmp leerF3
+leerF1:
+    mov dl, [fila1 + si]
+    jmp restaurarCaracter
+leerF2:
+    mov dl, [fila2 + si]
+    jmp restaurarCaracter
+leerF3:
+    mov dl, [fila3 + si]
+
+restaurarCaracter:
     mov ah, 2
-    mov dl, ' '  ; Reemplazamos la X por un espacio
     int 21h
 
-    ;=========================
-    ; DETECTAR FLECHAS
-    ;=========================
+    ;CONTROL DE TECLAS:
 
-    cmp scanCode, 48h ; FLECHA ARRIBA
-    jne noArriba
-    jmp arriba
-noArriba:
+    cmp scanCode, 48h   ;FLECHA ARRIBA
+    je arriba
 
-    cmp scanCode, 50h ; FLECHA ABAJO
-    jne noAbajo
-    jmp abajo
-noAbajo:
+    cmp scanCode, 50h   ;FLECHA ABAJO
+    je abajo
 
-    cmp scanCode, 4Bh ; FLECHA IZQUIERDA
-    jne noIzquierda
-    jmp izquierda
-noIzquierda:
+    cmp scanCode, 4Bh   ;FLECHA IZQUIERDA
+    je izquierda
 
-    cmp scanCode, 4Dh ; FLECHA DERECHA
-    jne noDerecha
-    jmp derecha
-noDerecha:
+    cmp scanCode, 4Dh   ;FLECHA DERECHA
+    je derecha
+
+    cmp scanCode, 1Ch   ;ENTER
+    je colocarFicha
 
     jmp gameLoop
 
-;---------------------------------
-;           MOVIMIENTOS
-;---------------------------------
-
+    ;CONTROL DE MOVIMIENTOS (LIMITADO PARA NO SALIRSE DEL TABLERO)
 volver:
     jmp gameLoop
 
 arriba:
-    cmp filaTablero, 0
+    cmp filTablero, 0
     je volver
-    dec filaTablero
+    dec filTablero
     jmp volver
 
 abajo:
-    cmp filaTablero, 2
+    cmp filTablero, 2
     je volver
-    inc filaTablero
+    inc filTablero
     jmp volver
 
 izquierda:
-    cmp columnaTablero, 0
+    cmp colTablero, 0
     je volver
-    dec columnaTablero
+    dec colTablero
     jmp volver
 
 derecha:
-    cmp columnaTablero, 2
+    cmp colTablero, 2
     je volver
-    inc columnaTablero
+    inc colTablero
     jmp volver
 
-main endp
+    ;PASAMOS A LA PARTE DE LA JUGADA DEL USUARIO (INGRESO DE FICHA)
+
+colocarFicha:
+    mov al, colTablero
+    mov bl, 4
+    mul bl
+    add al, 1          
+    xor ah, ah
+    mov si, ax ;USAMOS 'SI' PARA MANEJAR LOS INDICES Y SABER QUÉ CASILLERO ALTERAR
+
+    ;VERIFICAR DONDE ESTOY PARADO
+    cmp filTablero, 0
+    je f1
+    cmp filTablero, 1
+    je f2
+    jmp f3
+
+f1:
+    cmp [fila1 + si], ' ' ;COMPROBAMOS SI ESTÁ LIBRE
+    jne volver            ;EN CASO DE HABER UNA FICHA, NO PERMITE SUPERPONER
+    mov cl, fichaActual
+    mov [fila1 + si], cl  ;GUARDO FICHA
+    jmp cambiarTurno
+
+f2:
+    cmp [fila2 + si], ' ' 
+    jne volver
+    mov cl, fichaActual
+    mov [fila2 + si], cl
+    jmp cambiarTurno
+
+f3:
+    cmp [fila3 + si], ' '
+    jne volver
+    mov cl, fichaActual
+    mov [fila3 + si], cl
+    jmp cambiarTurno
+
+cambiarTurno:
+    cmp turno, 0
+    jne jugadorX
+    mov turno, 1
+    mov fichaActual, 'O'
+    jmp volver
+
+jugadorX:
+    mov turno, 0
+    mov fichaActual, 'X'
+    jmp volver
+
+    main endp
 end
